5352b0c8139e72bc48860a56b165531afc246199
Akshay <akshay@momentco.net>
2023-10-23 15:19:09 +0400

    Fixes

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node from Dev Branch!\n'
-  res.json({"message":msg});
+  res.send({"message":msg});
+  
 });
 
 server.listen(port, () => {
