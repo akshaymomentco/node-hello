d6d24d534aa1b638aae123ce2100a5325bc208cc
Akshay <akshay@momentco.net>
2023-10-23 15:13:09 +0400

    text and response change

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
-  res.end(msg);
+  const msg = 'Hello Node from Dev Branch!\n'
+  res.json({"message":msg});
 });
 
 server.listen(port, () => {
